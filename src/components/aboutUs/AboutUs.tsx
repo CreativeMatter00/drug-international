557c4d0581abb8274478e0c83dd03ba9c11078d9
GolamMuid <golammuid@gmail.com>
2023-12-17 17:58:49 +0600

    added company profile

---
+++ src/components/aboutUs/AboutUs.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { useState } from "react";
+import CompanyProfile from "./CompanyProfile";
+
+function AboutUs() {
+	const tabTitle = [
+		"company profile",
+		"Founder Chairman Profile",
+		"Message from MD",
+		"Mission & vision",
+		"Quality Policy",
+		"Corporate Social Responsibility",
+		"sister concerns",
+	];
+
+	const [selectedTab, setSelectedTab] = useState(0);
+
+	return (
+		<div className="absolute mt-[-100px] w-full">
+			<div className="container mx-auto bg-white py-14 px-12 rounded-2xl shadow-[0px_-2px_8px_0px_rgba(0,0,0,0.75)]">
+				<div className="flex items-center gap-4 text-center text-textPrimary overflow-x-auto mb-20">
+					{tabTitle.map((title: string, index: number) => (
+						<div
+							className={`uppercase py-4 px-2 h-[106px] flex items-center cursor-pointer rounded-md font-medium text-sm ${
+								selectedTab === index ? "bg-primaryShade text-white" : ""
+							}`}
+							key={index}
+							onClick={() => setSelectedTab(index)}
+						>
+							{title}
+						</div>
+					))}
+				</div>
+
+				<div className="border border-[#7B7B7B55] shadow-[0px_2px_5px_0px_rgba(0,0,0,0.3)] uppercase mb-16 py-6 rounded-md flex items-center justify-center gap-4 w-full">
+					<div className="border border-t-primaryShade w-[20%]" />
+					<div className="text-base font-medium">{tabTitle[selectedTab]}</div>
+					<div className="border border-t-primaryShade w-[20%]" />
+				</div>
+
+				{selectedTab === 0 && <CompanyProfile />}
+			</div>
+		</div>
+	);
+}
+
+export default AboutUs;
