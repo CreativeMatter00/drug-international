190012e3c74969413e7d79ddb957d977d8520d4a
Mahfuzislam1695 <mahfuzislam1695@gmail.com>
2023-12-18 14:53:35 +0600

    shadow

--- src/components/share/CapsuleButton.tsx
+++ src/components/share/CapsuleButton.tsx
@@ -1,18 +1,51 @@
+"use client";
+
+import styles from "@/styles/Test.module.css";
+import { useEffect, useState } from "react";
+
 interface CapsuleButtonProps {
   buttonName: string;
 }
 
 const CapsuleButton: React.FC<CapsuleButtonProps> = ({ buttonName }) => {
+  const [showCapsule, setShowCapsule] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowCapsule(true);
+    }, 3500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  console.log("showCapsule", showCapsule);
+
   return (
-    <button className="flex group rounded-full text-center justify-center items-center relative z-30">
-      <div className="bg-[#F00]  rounded-l-full shadow-xl z-30 w-[7vh] h-[5vh]"></div>
-      {/* <div className="bg-[#FF0000]  rounded-l-full shadow-xl z-30 w-[7vh] h-[5vh]"></div> */}
-      <div className="bg-[#D3D3D3] group-hover:translate-x-[8.5vh] duration-500 ease-in-out rounded-r-full shadow-xl z-10 w-[7vh] h-[5vh]"></div>
+    <button
+      className={`flex group rounded-full text-center justify-center items-center relative z-30 ${styles.boxShadow}`}
+    >
+      <div
+        className={`bg-[#FF0000]  rounded-l-full  z-30 w-[7vh] h-[5vh] `}
+      ></div>
+
+      <div
+        className={`bg-[#D3D3D3] duration-700 ease-in-out rounded-r-full  z-10 w-[7vh] h-[5vh] border border-[#c0bfbf] ${
+          showCapsule ? `translate-x-[8.5vh]` : ``
+        }`}
+      ></div>
 
       <div className={`left-0 absolute text-[3vh] overflow-hidden`}>
         <div className={`rounded-2xl ml-[7.5vh]`}>
-          <div className="w-0 group-hover:w-[10vh] text-left duration-500 opacity-0 group-hover:opacity-100 ease-in-out scale-75 group-hover:scale-100">
-            <p className="-ml-[5vh] group-hover:ml-0 duration-500 text-[#808080]">
+          <div
+            className={`w-0 text-left duration-500 opacity-0 ease-in-out scale-75 ${
+              showCapsule ? `w-[10vh] opacity-100 scale-100` : ``
+            }`}
+          >
+            <p
+              className={`-ml-[5vh] duration-700 text-[#808080] ${
+                showCapsule ? `ml-0` : ``
+              }`}
+            >
               {buttonName}
             </p>
           </div>
@@ -24,15 +57,24 @@ const CapsuleButton: React.FC<CapsuleButtonProps> = ({ buttonName }) => {
 
 export default CapsuleButton;
 
+// import styles from "@/styles/Test.module.css";
+
 // interface CapsuleButtonProps {
 //   buttonName: string;
 // }
 
 // const CapsuleButton: React.FC<CapsuleButtonProps> = ({ buttonName }) => {
 //   return (
-//     <button className="flex group rounded-full text-center justify-center items-center relative z-30">
-//       <div className="bg-[#FF0000]  rounded-l-full shadow-xl z-30 w-[7vh] h-[5vh]"></div>
-//       <div className="bg-[#D3D3D3] group-hover:translate-x-[8.5vh] duration-500 ease-in-out rounded-r-full shadow-xl z-10 w-[7vh] h-[5vh]"></div>
+//     <button
+//       className={`flex group rounded-full text-center justify-center items-center relative z-30 ${styles.boxShadow}`}
+//     >
+//       <div
+//         className={`bg-[#FF0000]  rounded-l-full  z-30 w-[7vh] h-[5vh] `}
+//       ></div>
+
+//       <div
+//         className={`bg-[#D3D3D3] group-hover:translate-x-[8.5vh] duration-500 ease-in-out rounded-r-full  z-10 w-[7vh] h-[5vh] border border-[#c0bfbf] `}
+//       ></div>
 
 //       <div className={`left-0 absolute text-[3vh] overflow-hidden`}>
 //         <div className={`rounded-2xl ml-[7.5vh]`}>
--- src/components/ui/navSidebar/Sidebar.tsx
+++ src/components/ui/navSidebar/Sidebar.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import styles from "@/styles/NavSidebar.module.css";
 
 const Sidebar = () => {
   const [isSidebarVisible, setSidebarVisible] = useState(
@@ -23,11 +24,11 @@ const Sidebar = () => {
     <div
       className={`${
         isSidebarVisible
-          ? "ease-in-out duration-700 left-0 shadow-lg shadow-gray-500/50"
+          ? `ease-in-out duration-700 left-0 ${styles.navShadow}`
           : "ease-in-out duration-700 -left-20 "
-      } bg-head text-white h-screen fixed w-20 z-40 overflow-hidden`}
+      } text-white h-screen fixed w-20 z-40 overflow-hidden`}
     >
-      <div className="w-20 h-screen border bg-white relative">
+      <div className="w-20 h-screen border bg-white relative ">
         <div className="flex h-full text-black justify-center items-center flex-col -mt-16 gap-2">
           <div className="group cursor-pointer flex items-center flex-col uppercase font-bold text-sm text-[#CC7A2D]">
             <Image
@@ -36,7 +37,6 @@ const Sidebar = () => {
               height={44}
               alt="Herbals"
               className="h-auto group-hover:hidden"
-              // className="h-auto block hover:hidden"
             />
             <Image
               src={`/assets/icons/sidebar/Herbal hovers.svg`}
@@ -68,13 +68,13 @@ const Sidebar = () => {
         </div>
 
         <button
-          className={` left-0 top-[50vh] text-black rounded-r-md p-0.5 fixed z-50 bg-white
+          className={`left-0 top-[48vh] -ml-0.5 text-black rounded-r-md p-0.5 fixed z-50 bg-white
             ${
               isSidebarVisible
                 ? "max-md:ml-0 ease-in-out duration-700 left-20"
                 : "max-md:pl-0 ease-in-out duration-700 left-0"
             }
-            shadow-lg shadow-gray-500/50`}
+            ${styles.navShadow}`}
           onClick={handleSidebarToggle}
         >
           {isSidebarVisible ? (
@@ -115,3 +115,120 @@ const Sidebar = () => {
 };
 
 export default Sidebar;
+// "use client";
+
+// import Image from "next/image";
+// import { useEffect, useState } from "react";
+
+// const Sidebar = () => {
+//   const [isSidebarVisible, setSidebarVisible] = useState(
+//     window.innerWidth > 767
+//   );
+
+//   const handleSidebarToggle = () => {
+//     setSidebarVisible(!isSidebarVisible);
+//   };
+
+//   useEffect(() => {
+//     const handleResize = () => {
+//       setSidebarVisible(window.innerWidth > 767);
+//     };
+//     window.addEventListener("resize", handleResize);
+//     return () => window.removeEventListener("resize", handleResize);
+//   }, []);
+//   return (
+//     <div
+//       className={`${
+//         isSidebarVisible
+//           ? "ease-in-out duration-700 left-0 shadow-lg shadow-gray-500/50"
+//           : "ease-in-out duration-700 -left-20 "
+//       } bg-head text-white h-screen fixed w-20 z-40 overflow-hidden`}
+//     >
+//       <div className="w-20 h-screen border bg-white relative">
+//         <div className="flex h-full text-black justify-center items-center flex-col -mt-16 gap-2">
+//           <div className="group cursor-pointer flex items-center flex-col uppercase font-bold text-sm text-[#CC7A2D]">
+//             <Image
+//               src={`/assets/icons/sidebar/Herbals.svg`}
+//               width={44}
+//               height={44}
+//               alt="Herbals"
+//               className="h-auto group-hover:hidden"
+//               // className="h-auto block hover:hidden"
+//             />
+//             <Image
+//               src={`/assets/icons/sidebar/Herbal hovers.svg`}
+//               width={44}
+//               height={44}
+//               alt="Herbal hovers"
+//               className="h-auto hidden group-hover:block"
+//             />
+//             HERBAL
+//           </div>
+
+//           <div className="group cursor-pointer flex items-center flex-col uppercase font-bold text-sm text-[#159A48]">
+//             <Image
+//               src={`/assets/icons/sidebar/Unanis.svg`}
+//               width={44}
+//               height={44}
+//               alt="Unanis"
+//               className="h-auto group-hover:hidden"
+//             />
+//             <Image
+//               src={`/assets/icons/sidebar/Unanis Hovers.svg`}
+//               width={44}
+//               height={44}
+//               alt="Unanis Hovers"
+//               className="h-auto hidden group-hover:block"
+//             />
+//             Unani
+//           </div>
+//         </div>
+
+//         <button
+//           className={` left-0 top-[50vh] text-black rounded-r-md p-0.5 fixed z-50 bg-white
+//             ${
+//               isSidebarVisible
+//                 ? "max-md:ml-0 ease-in-out duration-700 left-20"
+//                 : "max-md:pl-0 ease-in-out duration-700 left-0"
+//             }
+//             shadow-lg shadow-gray-500/50`}
+//           onClick={handleSidebarToggle}
+//         >
+//           {isSidebarVisible ? (
+//             <svg
+//               xmlns="http://www.w3.org/2000/svg"
+//               width="21"
+//               height="22"
+//               viewBox="0 0 21 22"
+//               fill="none"
+//             >
+//               <path
+//                 d="M13.012 5.75781L7.83154 10.9382L13.012 16.1187"
+//                 stroke="#272727"
+//                 stroke-width="1.29511"
+//                 stroke-linecap="round"
+//               />
+//             </svg>
+//           ) : (
+//             <svg
+//               xmlns="http://www.w3.org/2000/svg"
+//               width="21"
+//               height="22"
+//               viewBox="0 0 21 22"
+//               fill="none"
+//             >
+//               <path
+//                 d="M7.90258 16.1182L13.083 10.9377L7.90258 5.75731"
+//                 stroke="#272727"
+//                 stroke-width="1.29511"
+//                 stroke-linecap="round"
+//               />
+//             </svg>
+//           )}
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Sidebar;
