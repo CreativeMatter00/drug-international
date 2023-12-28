0f674eaa1e17c3296190467f788019e95748f5d6
Mahfuzislam1695 <mahfuzislam1695@gmail.com>
2023-12-28 18:40:46 +0600

    responsive footer

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -5,7 +5,6 @@ import Sidebar from "@/components/ui/navSidebar/Sidebar";
 import React, { ReactNode } from "react";
 
 function layout({ children }: { children: ReactNode }) {
-<<<<<<< HEAD
   return (
     <div>
       <Navbar />
@@ -13,20 +12,10 @@ function layout({ children }: { children: ReactNode }) {
         <Sidebar />
       </div>
 
-      <div className="mt-16">{children}</div>
+      <div className="mt-20">{children}</div>
       <Footer />
     </div>
   );
-=======
-	return (
-		<div>
-			<Navbar />
-			<Sidebar />
-			<div className="mt-20">{children}</div>
-			<Footer />
-		</div>
-	);
->>>>>>> c46700995310becc2537c07600bcbc6b5b8cb288
 }
 
 export default layout;
