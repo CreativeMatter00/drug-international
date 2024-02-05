cdd94baa402c0fdc962f6fe60f764a1d125a8953
Mahfuzislam1695 <mahfuzislam1695@gmail.com>
2024-02-05 16:59:01 +0600

    add tooltip1

--- src/components/products/allProducts/Herbal.tsx
+++ src/components/products/allProducts/Herbal.tsx
@@ -9,13 +9,9 @@ function Herbal() {
     queryFn: getHerbalProducts,
   });
 
-  console.log(data);
-
-  console.log("dataHerbal", data);
-
   return (
     <div className="px-5 py-10 mb-10">
-      <div className="grid grid-cols-2 md:grid-cols-5 gap-x-3 gap-y-5">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-x-3 gap-y-5">
         {data?.map((pharmaceutical: any) => (
           <div
             className="border border-[#e4e4e6] primaryShadow rounded-md w-full transition-all cursor-pointer hover:border-primary hover:scale-90 flex items-center justify-center"
--- src/components/products/allProducts/Oncology.tsx
+++ src/components/products/allProducts/Oncology.tsx
@@ -1,103 +1,103 @@
 import Image from "next/image";
 
 function Oncology() {
-	return (
-		<div className="px-5 py-10 mb-10">
-			<div className="grid grid-cols-2 md:grid-cols-5 gap-x-3 gap-y-5">
-				<div className="border border-[#e4e4e6] primaryShadow rounded-md w-full transition-all cursor-pointer hover:border-primary hover:scale-90">
-					<Image
-						src="/assets/images/products/all/oncology/1.png"
-						width={245}
-						height={165}
-						alt="product"
-						className="mx-auto"
-					/>
-				</div>
-				<div className="border border-[#e4e4e6] primaryShadow rounded-md w-full transition-all cursor-pointer hover:border-primary hover:scale-90">
-					<Image
-						src="/assets/images/products/all/oncology/2.png"
-						width={245}
-						height={165}
-						alt="product"
-						className="mx-auto"
-					/>
-				</div>
-				<div className="border border-[#e4e4e6] primaryShadow rounded-md w-full transition-all cursor-pointer hover:border-primary hover:scale-90 ">
-					<Image
-						src="/assets/images/products/all/oncology/3.png"
-						width={245}
-						height={165}
-						alt="product"
-						className="mx-auto"
-					/>
-				</div>
-				<div className="border border-[#e4e4e6] primaryShadow rounded-md w-full transition-all cursor-pointer hover:border-primary hover:scale-90 ">
-					<Image
-						src="/assets/images/products/all/oncology/4.png"
-						width={245}
-						height={165}
-						alt="product"
-						className="mx-auto"
-					/>
-				</div>
-				<div className="border border-[#e4e4e6] primaryShadow rounded-md w-full transition-all cursor-pointer hover:border-primary hover:scale-90 ">
-					<Image
-						src="/assets/images/products/all/oncology/5.png"
-						width={245}
-						height={165}
-						alt="product"
-						className="mx-auto"
-					/>
-				</div>
+  return (
+    <div className="px-5 py-10 mb-10">
+      <div className="grid grid-cols-2 md:grid-cols-5 gap-x-3 gap-y-5">
+        <div className="border border-[#e4e4e6] primaryShadow rounded-md w-full transition-all cursor-pointer hover:border-primary hover:scale-90">
+          <Image
+            src="/assets/images/products/all/oncology/1.png"
+            width={245}
+            height={165}
+            alt="product"
+            className="mx-auto"
+          />
+        </div>
+        <div className="border border-[#e4e4e6] primaryShadow rounded-md w-full transition-all cursor-pointer hover:border-primary hover:scale-90">
+          <Image
+            src="/assets/images/products/all/oncology/2.png"
+            width={245}
+            height={165}
+            alt="product"
+            className="mx-auto"
+          />
+        </div>
+        <div className="border border-[#e4e4e6] primaryShadow rounded-md w-full transition-all cursor-pointer hover:border-primary hover:scale-90 ">
+          <Image
+            src="/assets/images/products/all/oncology/3.png"
+            width={245}
+            height={165}
+            alt="product"
+            className="mx-auto"
+          />
+        </div>
+        <div className="border border-[#e4e4e6] primaryShadow rounded-md w-full transition-all cursor-pointer hover:border-primary hover:scale-90 ">
+          <Image
+            src="/assets/images/products/all/oncology/4.png"
+            width={245}
+            height={165}
+            alt="product"
+            className="mx-auto"
+          />
+        </div>
+        <div className="border border-[#e4e4e6] primaryShadow rounded-md w-full transition-all cursor-pointer hover:border-primary hover:scale-90 ">
+          <Image
+            src="/assets/images/products/all/oncology/5.png"
+            width={245}
+            height={165}
+            alt="product"
+            className="mx-auto"
+          />
+        </div>
 
-				<div className="border border-[#e4e4e6] primaryShadow rounded-md w-full transition-all cursor-pointer hover:border-primary hover:scale-90 ">
-					<Image
-						src="/assets/images/products/all/oncology/6.png"
-						width={245}
-						height={165}
-						alt="product"
-						className="mx-auto"
-					/>
-				</div>
-				<div className="border border-[#e4e4e6] primaryShadow rounded-md w-full transition-all cursor-pointer hover:border-primary hover:scale-90 ">
-					<Image
-						src="/assets/images/products/all/oncology/7.png"
-						width={245}
-						height={165}
-						alt="product"
-						className="mx-auto"
-					/>
-				</div>
-				<div className="border border-[#e4e4e6] primaryShadow rounded-md w-full transition-all cursor-pointer hover:border-primary hover:scale-90 ">
-					<Image
-						src="/assets/images/products/all/oncology/8.png"
-						width={245}
-						height={165}
-						alt="product"
-						className="mx-auto"
-					/>
-				</div>
-				<div className="border border-[#e4e4e6] primaryShadow rounded-md w-full transition-all cursor-pointer hover:border-primary hover:scale-90 ">
-					<Image
-						src="/assets/images/products/all/oncology/9.png"
-						width={245}
-						height={165}
-						alt="product"
-						className="mx-auto"
-					/>
-				</div>
-				<div className="border border-[#e4e4e6] primaryShadow rounded-md w-full transition-all cursor-pointer hover:border-primary hover:scale-90 ">
-					<Image
-						src="/assets/images/products/all/oncology/10.png"
-						width={245}
-						height={165}
-						alt="product"
-						className="mx-auto"
-					/>
-				</div>
-			</div>
-		</div>
-	);
+        <div className="border border-[#e4e4e6] primaryShadow rounded-md w-full transition-all cursor-pointer hover:border-primary hover:scale-90 ">
+          <Image
+            src="/assets/images/products/all/oncology/6.png"
+            width={245}
+            height={165}
+            alt="product"
+            className="mx-auto"
+          />
+        </div>
+        <div className="border border-[#e4e4e6] primaryShadow rounded-md w-full transition-all cursor-pointer hover:border-primary hover:scale-90 ">
+          <Image
+            src="/assets/images/products/all/oncology/7.png"
+            width={245}
+            height={165}
+            alt="product"
+            className="mx-auto"
+          />
+        </div>
+        <div className="border border-[#e4e4e6] primaryShadow rounded-md w-full transition-all cursor-pointer hover:border-primary hover:scale-90 ">
+          <Image
+            src="/assets/images/products/all/oncology/8.png"
+            width={245}
+            height={165}
+            alt="product"
+            className="mx-auto"
+          />
+        </div>
+        <div className="border border-[#e4e4e6] primaryShadow rounded-md w-full transition-all cursor-pointer hover:border-primary hover:scale-90 ">
+          <Image
+            src="/assets/images/products/all/oncology/9.png"
+            width={245}
+            height={165}
+            alt="product"
+            className="mx-auto"
+          />
+        </div>
+        <div className="border border-[#e4e4e6] primaryShadow rounded-md w-full transition-all cursor-pointer hover:border-primary hover:scale-90 ">
+          <Image
+            src="/assets/images/products/all/oncology/10.png"
+            width={245}
+            height={165}
+            alt="product"
+            className="mx-auto"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Oncology;
