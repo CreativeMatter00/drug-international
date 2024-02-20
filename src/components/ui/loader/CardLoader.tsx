a290302a8d2fc79cf78219beb50203e23643e505
Mahfuzislam1695 <404 page complete>
2024-02-20 14:50:35 +0600

    card loader

---
+++ src/components/ui/loader/CardLoader.tsx
@@ -0,0 +1,32 @@
+import CardLoaderCard from "./CardLoaderCard";
+
+const CardLoader = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="container flex gap-4">
+        <CardLoaderCard />
+        <CardLoaderCard />
+        <CardLoaderCard />
+        <CardLoaderCard />
+        <CardLoaderCard />
+        <CardLoaderCard />
+        <CardLoaderCard />
+        <CardLoaderCard />
+        <CardLoaderCard />
+        <CardLoaderCard />
+        <CardLoaderCard />
+        <CardLoaderCard />
+        <CardLoaderCard />
+        <CardLoaderCard />
+        <CardLoaderCard />
+        <CardLoaderCard />
+        <CardLoaderCard />
+        <CardLoaderCard />
+        <CardLoaderCard />
+        <CardLoaderCard />
+      </div>
+    </div>
+  );
+};
+
+export default CardLoader;
