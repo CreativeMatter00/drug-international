bdfc8581d945d3662342e78f8c687843c1d9904f
Mahfuzislam1695 <404 page complete>
2024-03-14 14:44:50 +0600

    lightbox image gallery

--- src/components/facilities/Unit3.tsx
+++ src/components/facilities/Unit3.tsx
@@ -2,47 +2,95 @@
 
 import { motion } from "framer-motion";
 import { useTranslations } from "next-intl";
-import Image from "next/image";
+import { SlideshowLightbox } from "lightbox.js-react";
+import "lightbox.js-react/dist/index.css";
 
 function Unit3() {
-	const t = useTranslations("Facilities");
+  const t = useTranslations("Facilities");
 
-	const slideInAnimationTop = {
-		hidden: { opacity: 0, y: 400 },
-		visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
-	};
-	return (
-		<motion.div
-			initial="hidden"
-			animate="visible"
-			variants={slideInAnimationTop}
-		>
-			<p className="font-medium text-3xl uppercase text-primary mb-10">
-				{t("unit3")}
-			</p>
+  const slideInAnimationTop = {
+    hidden: { opacity: 0, y: 400 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+  };
 
-			<div className=" grid grid-cols-2 mb-8">
-				<Image
-					src="/assets/images/facilities/unit3.jpg"
-					width={720}
-					height={405}
-					alt="product"
-					className="max-h-[700px] w-auto mx-auto rounded-md"
-				/>
-				<Image
-					src="/assets/images/facilities/unit3-2.jpg"
-					width={720}
-					height={405}
-					alt="product"
-					className="max-h-[700px] w-auto mx-auto rounded-md"
-				/>
-			</div>
+  const handleContextMenu = (event: any) => {
+    event.preventDefault();
+  };
+  return (
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      variants={slideInAnimationTop}
+    >
+      <p className="font-medium text-3xl uppercase text-primary mb-10">
+        {t("unit3")}
+      </p>
 
-			<div className="font-medium text-base text-textSecondary">
-				<p className=""></p>
-			</div>
-		</motion.div>
-	);
+      {/* <div className=" grid grid-cols-2 mb-8">
+        <Image
+          src="/assets/images/facilities/unit3.jpg"
+          width={720}
+          height={405}
+          alt="product"
+          className="max-h-[700px] w-auto mx-auto rounded-md"
+        />
+        <Image
+          src="/assets/images/facilities/unit3-2.jpg"
+          width={720}
+          height={405}
+          alt="product"
+          className="max-h-[700px] w-auto mx-auto rounded-md"
+        />
+      </div> */}
+
+      {/* <SlideshowLightbox className=" " showThumbnails={true} theme="lightbox"> */}
+      {/* <div className="bg-white rounded-lg overflow-hidden shadow-md transform transition-transform hover:scale-105 cursor-pointer"></div> */}
+      {/* <img className='w-full rounded' src={image} alt="Gallery" /> */}
+      {/* <Image
+        src="/assets/images/facilities/unit3-2.jpg"
+        width={720}
+        height={405}
+        alt="product"
+        className="max-h-[700px] w-auto mx-auto rounded-md"
+      />
+      <Image
+        src="/assets/images/facilities/unit3.jpg"
+        width={720}
+        height={405}
+        alt="product"
+        className="max-h-[700px] w-auto mx-auto rounded-md"
+      /> */}
+      {/* </SlideshowLightbox> */}
+
+      <SlideshowLightbox className="container grid grid-cols-1 lg:grid-cols-2 gap-8 mx-auto">
+        <img
+          className="w-full rounded-lg h-96"
+          //   src="https://source.unsplash.com/pAKCx4y2H6Q/1400x1200"
+          src="/assets/images/facilities/unit-3/unint_3_1.jpg"
+        />
+        <img
+          className="w-full rounded-lg h-96"
+          src="/assets/images/facilities/unit-3/unint_3_2.jpg"
+        />
+        <img
+          className="w-full rounded-lg h-96"
+          src="/assets/images/facilities/unit-3/unint_3_3.jpg"
+        />
+        <img
+          className="w-full rounded-lg h-96"
+          src="/assets/images/facilities/unit-3/unint_3_4.jpg"
+        />
+        <img
+          className="w-full rounded-lg h-96"
+          src="/assets/images/facilities/unit-3/unint_3_5.jpg"
+        />
+      </SlideshowLightbox>
+
+      <div className="font-medium text-base text-textSecondary">
+        <p className=""></p>
+      </div>
+    </motion.div>
+  );
 }
 
 export default Unit3;
