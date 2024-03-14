8b7770a155876c16aba17e9a59c2540b475fd178
Mahfuzislam1695 <mahfuzislam1695@gmail.com>
2024-03-14 15:24:02 +0600

    oncology lightbox gallery2

--- src/components/facilities/Unit3.tsx
+++ src/components/facilities/Unit3.tsx
@@ -38,7 +38,7 @@ function Unit3() {
         />
         <img
           className="w-full rounded-lg h-96"
-          src="/assets/images/facilities/unit-3/unint_3-311111.jpg"
+          src="/assets/images/facilities/unit-3/unint_3-3.jpg"
         />
         <img
           className="w-full rounded-lg h-96"
